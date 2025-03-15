21a2ca571619f1ba77fff0c8c67872b9af1d3edd
saif el_dein tarek <saifel.dein01010@gmail.com>
2025-03-15 02:42:05 +0200

    Add Read_number method

--- Read_Int_Input.cs
+++ Read_Int_Input.cs
@@ -6,7 +6,7 @@ namespace Generic_Libraries
     class Read_Int_Input
     {
 
-        public static bool IsIntNumber(string number)
+        public static bool IsNumber(string number)
         {
 
             return int.TryParse(number, out int i);
@@ -14,6 +14,34 @@ namespace Generic_Libraries
         }
 
 
+        public static int ReadNumber(string errorMessage = "Not vaild number, Enter a vaild number: ")
+        {
+
+            int number = 0;
+            string textNumber = Console.ReadLine();
+
+            while(!IsNumber(textNumber))
+            {
+
+                Console.Write(errorMessage);
+                textNumber = Console.ReadLine();
+
+            }
+
+            number = Convert.ToInt32(textNumber);
+
+            return number;
+
+        }
+
+
+        public static int ReadNumber(string message, string errorMessage = "Not vaild number, Enter a vaild number: ")
+        {
+
+            Console.Write(message);
+            return ReadNumber(errorMessage);
+
+        }
         
 
 
