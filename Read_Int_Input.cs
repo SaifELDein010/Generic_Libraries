ecefb7a10bdc49e162b33329096a3574c8358a53
saif el_dein tarek <saifel.dein01010@gmail.com>
2025-03-15 02:28:37 +0200

    Init Read int input, Add Is_Number method

---
+++ Read_Int_Input.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace Generic_Libraries
+{
+    class Read_Int_Input
+    {
+
+        public static bool IsIntNumber(string number)
+        {
+
+            return int.TryParse(number, out int i);
+
+        }
+
+
+        
+
+
+
+
+
+    }
+}
